75c3a385de9d4a52aeff12051e4995b03bafd380
nina <juan.amado.sicat.cleto@gmail.com>
2025-05-22 18:17:17 +0800

    Character Added and Person Added

--- src/Character.java
+++ src/Character.java
@@ -1,5 +1,10 @@
 public class Character extends Person {
 
+    private float angry;
+    private float sad;
+    private float happy;
+    private float neutral;
+
     public Character(String name, int age)
     {
         super(name, age);
@@ -20,6 +25,22 @@ public class Character extends Person {
         super.setAge(age);
     }
 
+    public void setAngry(float angry) {
+        this.angry = angry;
+    }
+
+    public void setSad(float sad) {
+        this.sad = sad;
+    }
+
+    public void setHappy(float happy) {
+        this.happy = happy;
+    }
+
+    public void setNeutral(float neutral) {
+        this.neutral = neutral;
+    }
+
     @Override
     public String getName() {
         return super.getName();
@@ -28,5 +49,26 @@ public class Character extends Person {
     @Override
     public int getAge() {
         return super.getAge();
+    }
+
+    public float getAngry() {
+        return angry;
+    }
+
+    public float getSad() {
+        return sad;
+    }
+
+    public float getHappy() {
+        return happy;
+    }
+
+    public float getNeutral() {
+        return neutral;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
     }
 }
