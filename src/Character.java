98e11ebae71b65322bd732eee540a5a58244708e
nina <juan.amado.sicat.cleto@gmail.com>
2025-05-22 18:14:09 +0800

    Character Added and Person Added

--- src/Character.java
+++ src/Character.java
@@ -1,4 +1,32 @@
-public class Character {
+public class Character extends Person {
 
+    public Character(String name, int age)
+    {
+        super(name, age);
+    }
 
+    public Character()
+    {
+
+    }
+
+    @Override
+    public void setName(String name) {
+        super.setName(name);
+    }
+
+    @Override
+    public void setAge(int age) {
+        super.setAge(age);
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    @Override
+    public int getAge() {
+        return super.getAge();
+    }
 }
